c22f2eecc34795d6a1f1371b09c03c36d003023c
Baptiste <baptiste.lecat44@gmail.com>
2021-05-09 17:42:13 +0200

    Create card-info.php
    View file for this element.

---
+++ view/card-info.php
@@ -0,0 +1,17 @@
+<div class="draggable-component" draggable="true" id="card-info">
+    <header>
+        <h1>Parcours</h1>
+    </header>
+    <div class="picture_container">
+        <button><img src="assets/icons/next.svg" alt=""></button>
+        <picture><img src="assets/img/course/024-landing page.svg" alt=""></picture>
+        <button><img src="assets/icons/next.svg" alt=""></button>
+    </div>
+    <div class="content">
+        <h5>BTS SIO</h5>
+        <h6>2019 / 2021</h6>
+    </div>
+    <div class="btn_container">
+        <button>En savoir plus</button>
+    </div>
+</div>
