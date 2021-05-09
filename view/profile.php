abd61c16ab3085d6e3eb37bae929745a17fbcd1b
Baptiste <baptiste.lecat44@gmail.com>
2021-05-09 19:26:41 +0200

    Profile view file

---
+++ view/profile.php
@@ -0,0 +1,10 @@
+    <div class="draggable-component" id="profile">
+        <picture><img src="assets/img/profile/baptisteLecat.png" alt=""></picture>
+        <div class="content">
+            <h5>Baptiste Lecat</h5>
+            <h6>19 ans</h6>
+        </div>
+        <div class="btn_container">
+            <button>Voir le CV</button>
+        </div>
+    </div>
