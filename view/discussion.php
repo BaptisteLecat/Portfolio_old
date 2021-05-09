ca42c709fb82a9bdaba00070b4b4a7091ff80893
Baptiste <baptiste.lecat44@gmail.com>
2021-05-09 22:44:47 +0200

    discussion view file

---
+++ view/discussion.php
@@ -0,0 +1,35 @@
+    <div class="draggable-component" id="discussion">
+        <header>
+            <h1>Discussion</h1>
+        </header>
+        <div class="message_wrapper">
+            <div class="message_container">
+                <picture><img src="assets/img/profile/baptisteLecat.png" alt=""></picture>
+                <div class="content">
+                    <h6>Baptiste Lecat</h6>
+                    <p>Bonjour je suis heureux de vous rencontrer, bienvenue sur mon Portfolio !</p>
+                </div>
+            </div>
+            <div class="message_container">
+                <picture><img src="assets/img/profile/baptisteLecat.png" alt=""></picture>
+                <div class="content">
+                    <h6>Baptiste Lecat</h6>
+                    <p>Passionné par l’informatique, le développement, l’UX et UI. Je souhaite vous proposez un expérience originale sur mon site.</p>
+                </div>
+            </div>
+            <div class="message_container">
+                <picture><img src="assets/img/profile/baptisteLecat.png" alt=""></picture>
+                <div class="content">
+                    <h6>Baptiste Lecat</h6>
+                    <p>Bonne visite !</p>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="input_container">
+                <button><img src="assets/icons/discussion/more.svg" alt=""></button>
+                <input type="text" name="message" id="message" placeholder="Ecrire un message">
+                <button><img src="assets/icons/discussion/message_circle.svg" alt=""></button>
+            </div>
+        </footer>
+    </div>
