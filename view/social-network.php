72210cb04b042814e14e12bff5c0114999d345a7
Baptiste <baptiste.lecat44@gmail.com>
2021-05-09 17:44:10 +0200

    Social-Network view file

---
+++ view/social-network.php
@@ -0,0 +1,11 @@
+    <div class="draggable-component" id="social-network">
+        <div class="icon_container">
+            <img src="assets/icons/social/youtube.svg" alt="">
+        </div>
+        <div class="icon_container">
+            <img src="assets/icons/social/linkedIn.svg" alt="">
+        </div>
+        <div class="icon_container">
+            <img src="assets/icons/social/github.svg" alt="">
+        </div>
+    </div>
