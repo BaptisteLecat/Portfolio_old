8bae16a85fba0fec267f98e64b29e8e306d4366f
Baptiste <baptiste.lecat44@gmail.com>
2021-05-13 01:14:13 +0200

    Project view + init page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/cardInfo/cardInfo.css">
+    <link rel="stylesheet" href="assets/css/social/socialNetwork.css">
+    <link rel="stylesheet" href="assets/css/profile/profile.css">
+    <link rel="stylesheet" href="assets/css/discussion/discussion.css">
+    <link rel="stylesheet" href="assets/css/project/project.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+    <title>Portfolio - Lecat Baptiste</title>
+</head>
+
+<body>
+
+    <?php
+
+    include("view/project.php");
+    include("view/discussion.php");
+    include("view/card-info.php");
+    include("view/card-info.php");
+    include("view/social-network.php");
+    include("view/profile.php");
+
+    ?>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ view/project.php
@@ -0,0 +1,125 @@
+<div class="draggable-component" id="project">
+        <nav>
+            <header>
+                <img src="assets/img/project/026-code.svg" alt="">
+                <h1>Projets</h1>
+            </header>
+            <ul>
+                <li class="selected">
+                    <img src="assets/img/project/navBar/027-web development.svg" alt="">
+                    <h6>Web</h6>
+                </li>
+                <li>
+                    <img src="assets/img/project/navBar/004-user interface.svg" alt="">
+                    <h6>Mobile</h6>
+                </li>
+                <li>
+                    <img src="assets/img/project/navBar/018-user interface.svg" alt="">
+                    <h6>Maquette</h6>
+                </li>
+                <li>
+                    <img src="assets/img/project/navBar/027-user interface.svg" alt="">
+                    <h6>Logiciel</h6>
+                </li>
+            </ul>
+        </nav>
+        <div class="app_container">
+            <header>
+                <h2>Découvrez tout mes projets Web</h2>
+            </header>
+            <section>
+                <h3>Languages utilisés</h3>
+                <div class="language_wrapper">
+                    <div class="language">
+                        <img src="assets/img/project/languages/027-php.svg" alt="">
+                        <h6>PHP</h6>
+                    </div>
+                    <div class="language">
+                        <img src="assets/img/project/languages/030-html-5.svg" alt="">
+                        <h6>HTML</h6>
+                    </div>
+                    <div class="language">
+                        <img src="assets/img/project/languages/027-php.svg" alt="">
+                        <h6>PHP</h6>
+                    </div>
+                    <div class="language">
+                        <img src="assets/img/project/languages/030-html-5.svg" alt="">
+                        <h6>HTML</h6>
+                    </div>
+                    <div class="language">
+                        <img src="assets/img/project/languages/027-php.svg" alt="">
+                        <h6>PHP</h6>
+                    </div>
+                    <div class="language">
+                        <img src="assets/img/project/languages/030-html-5.svg" alt="">
+                        <h6>HTML</h6>
+                    </div>
+                    <div class="language">
+                        <img src="assets/img/project/languages/027-php.svg" alt="">
+                        <h6>PHP</h6>
+                    </div>
+                    <div class="language">
+                        <img src="assets/img/project/languages/030-html-5.svg" alt="">
+                        <h6>HTML</h6>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h3>Mes projets</h3>
+                <div class="project_wrapper">
+                    <div class="project">
+                        <picture>
+                            <img src="assets/img/project/thumbnails/001-user interface.svg" alt="">
+                        </picture>
+                        <div class="content">
+                            <h6>Application Todo</h6>
+                            <p>Cette application web permet la gestion de tâches.</p>
+                        </div>
+                        <footer>
+                            <p>2020 / 2021</p>
+                        </footer>
+                    </div>
+
+                    <div class="project">
+                        <picture>
+                            <img src="assets/img/project/thumbnails/001-user interface.svg" alt="">
+                        </picture>
+                        <div class="content">
+                            <h6>Application Todo</h6>
+                            <p>Cette application web permet la gestion de tâches.</p>
+                        </div>
+                        <footer>
+                            <p>2020 / 2021</p>
+                        </footer>
+                    </div>
+
+                    <div class="project">
+                        <picture>
+                            <img src="assets/img/project/thumbnails/001-user interface.svg" alt="">
+                        </picture>
+                        <div class="content">
+                            <h6>Application Todo</h6>
+                            <p>Cette application web permet la gestion de tâches.</p>
+                        </div>
+                        <footer>
+                            <p>2020 / 2021</p>
+                        </footer>
+                    </div>
+
+                    <div class="project">
+                        <picture>
+                            <img src="assets/img/project/thumbnails/001-user interface.svg" alt="">
+                        </picture>
+                        <div class="content">
+                            <h6>Application Todo</h6>
+                            <p>Cette application web permet la gestion de tâches.</p>
+                        </div>
+                        <footer>
+                            <p>2020 / 2021</p>
+                        </footer>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    </div>
