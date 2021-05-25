22f40c82b46397dab49fca88d3584a50dc72a7d3
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-05-25 21:38:53 +0200

    Create SocialNetworkManager.php

---
+++ App/Model/Manager/SocialNetworkManager.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Model\Manager;
+
+use App\Model\Entity\SocialNetwork;
+use App\PdoFactory;
+use \PDOException;
+use \Exception;
+
+class SocialNetworkManager
+{
+    public static function selectSocialNetwork()
+    {
+        $list_socialNetwork = array();
+        try {
+            $request = PdoFactory::getPdo()->prepare("SELECT * FROM socialnetwork");
+            $request->execute();
+            while ($result = $request->fetch()) {
+                $socialNetwork = new SocialNetwork($result["socialnetwork_id"], $result["socialnetwork_label"], $result["socialnetwork_picture"], $result["socialnetwork_link"]);
+                array_push($list_socialNetwork, $socialNetwork);
+            }
+
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage());
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        return $socialNetwork;
+    }
+}
