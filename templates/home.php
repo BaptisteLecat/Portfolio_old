517ca71e1052091a4bfb6b3c1c25a9db93610e3a
Baptiste <baptiste.lecat44@gmail.com>
2021-05-27 01:36:28 +0200

    Re-organisation of the project
    Moving of all the assets in the public folder. Creation of a template folder, contains the module ("view" older).

---
+++ templates/home.php
@@ -0,0 +1,10 @@
+    <?php
+
+    include("module/project.php");
+    include("module/discussion.php");
+    include("module/card-info.php");
+    include("module/card-info.php");
+    include("module/social-network.php");
+    include("module/profile.php");
+
+    ?>
