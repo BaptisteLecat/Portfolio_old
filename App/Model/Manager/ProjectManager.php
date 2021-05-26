cf30aa9e8ef76a815db52a36cbe36c0ea1d0c9aa
Baptiste <baptiste.lecat44@gmail.com>
2021-05-26 16:39:45 +0200

    Update ProjectManager.php
    Correction function name

--- App/Model/Manager/ProjectManager.php
+++ App/Model/Manager/ProjectManager.php
@@ -18,7 +18,7 @@ class ProjectManager
             $request->execute();
             while ($result = $request->fetch()) {
                 //Load data of category.
-                $category = CategoryManager::selectCategoryFromId($result["project_idCategory"]);
+                $category = CategoryManager::selectCategoryFromId($result["project_categoryId"]);
                 $project = new Project($result["project_id"], $result["project_title"], $result["project_content"], $result["project_picture"], $result["project_startdate"], $result["project_enddate"], $category);
                 array_push($list_project, $project);
             }
@@ -31,10 +31,10 @@ class ProjectManager
         return $list_project;
     }
 
-    public static function loadTechnologyForCategory(Project $project)
+    public static function loadTechnologyForProject(Project $project)
     {
         try {
-            $request = PdoFactory::getPdo()->prepare("SELECT technology.technology_id, technology.technology_label, technology.technology_picture FROM technology, project_technology WHERE technology.technology_id = project_technology.technology_id AND project_technology.category_id = :project_id");
+            $request = PdoFactory::getPdo()->prepare("SELECT technology.technology_id, technology.technology_label, technology.technology_picture FROM technology, project_technology WHERE technology.technology_id = project_technology.technology_id AND project_technology.project_id = :project_id");
             $request->execute(array(':project_id' => $project->getId()));
             while ($result = $request->fetch()) {
                 $technology = new Technology($result["technology_id"], $result["technology_label"], $result["technology_picture"]);
