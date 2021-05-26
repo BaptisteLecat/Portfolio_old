e04ce676336e271988d0d5ebeecd11cb2892e0a7
Baptiste <baptiste.lecat44@gmail.com>
2021-05-26 16:39:10 +0200

    Update CategoryManager.php

--- App/Model/Manager/CategoryManager.php
+++ App/Model/Manager/CategoryManager.php
@@ -6,6 +6,7 @@ use Exception;
 use PDOException;
 use App\PdoFactory;
 use App\Model\Entity\Category;
+use App\Model\Entity\Project;
 use App\Model\Entity\Technology;
 
 class CategoryManager
@@ -29,13 +30,28 @@ class CategoryManager
         return $list_category;
     }
 
+    public static function loadTechnologyForCategory(Category $category)
+    {
+        try {
+            $request = PdoFactory::getPdo()->prepare("SELECT technology.technology_id, technology.technology_label, technology.technology_picture FROM technology, category_technology WHERE technology.technology_id = category_technology.technology_id AND category_technology.category_id = :category_id");
+            $request->execute(array(':category_id' => $category->getId()));
+            while ($result = $request->fetch()) {
+                $technology = new Technology($result["technology_id"], $result["technology_label"], $result["technology_picture"]);
+                $category->addTechnology($technology);
+            }
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage());
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
 
     public static function selectCategoryFromId(int $categoryId)
     {
         $category = null;
         try {
             $request = PdoFactory::getPdo()->prepare("SELECT * FROM category WHERE category_id = :category_id");
-            $request->execute();
+            $request->execute(array(":category_id" => $categoryId));
             if($request->rowCount() > 0){
                 $result = $request->fetch();
                 $category = new Category($result["category_id"], $result["category_label"], $result["category_picture"]);
@@ -49,14 +65,14 @@ class CategoryManager
         return $category;
     }
 
-    public static function loadTechnologyForCategory(Category $category)
+    public static function loadProjectForCategory(Category $category)
     {
         try {
-            $request = PdoFactory::getPdo()->prepare("SELECT technology.technology_id, technology.technology_label, technology.technology_picture FROM technology, category_technology WHERE technology.technology_id = category_technology.technology_id AND category_technology.category_id = :category_id");
-            $request->execute(array(':category_id' => $category->getId()));
+            $request = PdoFactory::getPdo()->prepare("SELECT project.project_id, project.project_title, project.project_content, project.project_picture, project.project_startdate, project.project_enddate FROM project WHERE project.project_categoryId = :project_categoryId");
+            $request->execute(array(':project_categoryId' => $category->getId()));
             while ($result = $request->fetch()) {
-                $technology = new Technology($result["technology_id"], $result["technology_label"], $result["technology_picture"]);
-                $category->addTechnology($technology);
+                $project = new Project($result["project_id"], $result["project_title"], $result["project_content"], $result["project_picture"], $result["project_startdate"], $result["project_enddate"], $category);
+                $category->addProject($project);
             }
         } catch (PDOException $e) {
             throw new PDOException($e->getMessage());
