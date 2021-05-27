92d19671682ec6617bc278ef337f8c2cc5eb2f41
Baptiste <baptiste.lecat44@gmail.com>
2021-05-27 11:35:38 +0200

    Create HomeController.php

---
+++ App/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+class HomeController extends MainController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->loadAll();
+    }
+
+    public function display()
+    {
+        foreach ($this->list_activity as $activity) {
+            require("../templates/module/card-info.php");
+        }
+    }
+}
