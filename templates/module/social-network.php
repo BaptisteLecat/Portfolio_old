ebf8ca6d6dacc4f42b1d1521369a542d948ee5ce
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-05-28 17:50:49 +0200

    Update social-network.php
    Link to open a new window with the socialNetwork.

--- templates/module/social-network.php
+++ templates/module/social-network.php
@@ -1,7 +1,7 @@
     <div class="draggable-component" id="social-network">
     <?php foreach($this->list_socialNetwork as $socialNetwork){ ?>
-        <div class="icon_container">
-            <img src="assets/icons/social/<?= $socialNetwork->getPicture(); ?>" alt="">
+        <div class="icon_container" title="<?= $socialNetwork->getLabel(); ?>" onclick="window.open('<?= $socialNetwork->getLink()?>');">
+            <img src="assets/icons/social/<?= $socialNetwork->getPicture(); ?>">
         </div>
         <?php } ?>
     </div>
