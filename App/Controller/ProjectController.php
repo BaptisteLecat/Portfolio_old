c55f4723acc6f10072e1b6f26f7063e66c39918e
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-05-28 18:31:22 +0200

    Create ProjectController.php
    Handler for the project module.

---
+++ App/Controller/ProjectController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+class ProjectController extends MainController
+{
+    private $selectedCategory;
+
+    public function __construct($selectedCategory = null)
+    {
+        parent::__construct();
+        $this->loadProjectInfo();
+        $this->selectedCategory = (is_null($selectedCategory)) ? 0 : $selectedCategory; 
+    }
+
+    public function setSelectedCategory($selectedCategory)
+    {
+        if(!filter_var($selectedCategory, FILTER_VALIDATE_INT)){
+            $this->selectedCategory = 0;
+        }else{
+            if($selectedCategory < 0 || $selectedCategory > (count($this->list_category) - 1)){
+                $this->selectedCategory = 0;
+            }else{
+                $this->selectedCategory = $selectedCategory;
+            }
+        }
+    }
+
+    private function loadProjectInfo(){
+        $this->loadProject();
+        $this->loadTechnology();
+        $this->loadCategory();
+    }
+
+    public function display()
+    {
+        require("../templates/module/project.php");
+    }
+
+    private function technologiesForCategory(){
+        return $this->list_category[$this->selectedCategory]->getList_Technology();
+    }
+
+    private function projectsForCategory(){
+        $list_project = array();
+
+        foreach ($this->list_project as $project) {
+            if($project->getCategory()->getId() == $this->list_category[$this->selectedCategory]->getId()){
+                array_push($list_project, $project);
+            }
+        }
+
+        return $list_project;
+    }
+}
