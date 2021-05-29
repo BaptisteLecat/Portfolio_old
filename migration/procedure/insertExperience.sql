b73c34906411f5b0c7c6f090499a87ac750bf682
Baptiste <baptiste.lecat44@gmail.com>
2021-05-29 18:38:54 +0200

    Update Date type
    Now date is a VARCHAR(255)

--- migration/procedure/insertExperience.sql
+++ migration/procedure/insertExperience.sql
@@ -5,7 +5,7 @@
 
 DROP PROCEDURE IF EXISTS INSERT_EXPERIENCE;
 DELIMITER |
-CREATE PROCEDURE INSERT_EXPERIENCE (IN _title VARCHAR(255), IN _date DATE, _picture VARCHAR(255))  
+CREATE PROCEDURE INSERT_EXPERIENCE (IN _title VARCHAR(255), IN _date VARCHAR(255), _picture VARCHAR(255))  
 BEGIN
 	DECLARE _activityId integer unsigned default null; -- valeur de retour pour connaitre l'id de l'insertion.
 
